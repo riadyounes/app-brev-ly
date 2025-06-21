87e7cc1539157a6722db9c58a793d6432e33e618
Riad Younes <riad.younes@hotmail.com>
2025-06-21 01:34:36 -0300

    Add LinkList and Button components for link management and CSV download

---
+++ brev-ly-web/src/components/link-list.tsx
@@ -0,0 +1,25 @@
+import { DownloadSimpleIcon, Link } from "@phosphor-icons/react/dist/ssr";
+import { Button } from "./ui/button";
+
+export function LinkList() {
+
+  const isEmptyList = true; // This should be replaced with actual logic to check if there are links
+  return (
+    <div className="max-w-[580px] w-full rounded-lg bg-gray-100 p-8 flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold text-2xl text-gray-600">Meus links</h2>
+        <Button variant="secondary" className="flex items-center" >
+          <DownloadSimpleIcon className="size-4 mr-1.5" />
+            Baixar CSV
+        </Button>
+      </div>
+      <div className="w-full border-b border-gray-200" />
+      {isEmptyList && (
+        <div className="flex flex-col items-center justify-center gap-3 py-4">
+          <Link className="text-gray-400 size-8" />
+          <p className="text-sm text-gray-500 uppercase">ainda não existem links cadastrados</p>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ brev-ly-web/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { LinkList } from "../components/link-list";
+
+export function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <LinkList />
+    </div>
+  )
+}
