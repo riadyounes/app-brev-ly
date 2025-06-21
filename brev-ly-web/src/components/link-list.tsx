87062ec748fbe734b588192ee954012d7ef7a3e8
Riad Younes <riad.younes@hotmail.com>
2025-06-21 02:08:52 -0300

    Update index.html and Home component layout for improved asset management and styling

--- brev-ly-web/src/components/link-list.tsx
+++ brev-ly-web/src/components/link-list.tsx
@@ -5,7 +5,7 @@ export function LinkList() {
 
   const isEmptyList = true; // This should be replaced with actual logic to check if there are links
   return (
-    <div className="max-w-[580px] w-full rounded-lg bg-gray-100 p-8 flex flex-col gap-4">
+    <div className="w-full rounded-lg bg-gray-100 p-8 flex flex-col gap-4">
       <div className="flex items-center justify-between">
         <h2 className="font-bold text-2xl text-gray-600">Meus links</h2>
         <Button variant="secondary" className="flex items-center" >
@@ -17,7 +17,7 @@ export function LinkList() {
       {isEmptyList && (
         <div className="flex flex-col items-center justify-center gap-3 py-4">
           <Link className="text-gray-400 size-8" />
-          <p className="text-sm text-gray-500 uppercase">ainda não existem links cadastrados</p>
+          <p className="text-xs text-gray-500 uppercase">ainda não existem links cadastrados</p>
         </div>
       )}
     </div>
--- brev-ly-web/src/pages/home.tsx
+++ brev-ly-web/src/pages/home.tsx
@@ -1,9 +1,20 @@
 import { LinkList } from "../components/link-list";
+import Logo from "../assets/Logo.svg";
 
 export function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <LinkList />
+    <div className="min-h-screen bg-gray-300">
+      <div className="max-w-7xl flex flex-col p-8 justify-center items-center md:items-start">
+        <img src={Logo} alt="" className="h-6 w-[96px] mb-6 md:mb-8" />
+        <div className="flex flex-col md:flex-row gap-5 w-full">
+          <div className="bg-gray-100 p-6 rounded-lg w-full">
+            <h2 className="text-xl font-semibold mb-4">Novo link</h2>
+          </div>
+          <LinkList />
+        </div>
+      </div>
+
     </div>
+   
   )
 }
