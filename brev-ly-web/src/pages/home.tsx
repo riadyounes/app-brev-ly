3f8e78f645faa6f493e3c51de9cf8bf1674c0672
Riad Younes <riad.younes@hotmail.com>
2025-06-22 22:02:32 -0300

    Enhance responsiveness of LinkList and Home components by adjusting max-width classes

--- brev-ly-web/src/pages/home.tsx
+++ brev-ly-web/src/pages/home.tsx
@@ -8,7 +8,7 @@ export function Home() {
       <div className="max-w-[980px] mx-auto flex flex-col pt-8 px-3 md:pt-20 justify-center items-center md:items-start">
         <img src={Logo} alt="" className="h-6 w-[96px] mb-6 md:mb-8" />
         <div className="flex flex-col md:flex-row gap-3 md:gap-5 w-full">
-          <div className="max-w-[380px] bg-gray-100 p-6 rounded-lg w-full">
+          <div className="md:max-w-[380px] bg-gray-100 p-6 rounded-lg w-full">
             <h2 className="text-xl font-semibold mb-4">Novo link</h2>
             <Button className="w-full">Salvar link</Button>
           </div>
